99f23b9b1cf5d946f1d9bb2a76f42b4c160e3972
Matthew Pieczynski <matthew.n.pieczynski00@account.cybbh.space>
2022-10-24 09:27:16 -0400

    Add multiply and divide functions to be used

--- src/operands.c
+++ src/operands.c
@@ -23,11 +23,15 @@ static int32_t find_greater(int32_t num_1, int32_t num_2)
     return num_1 > num_2 ? num_1 : num_2;
 }
 
+static bool check_for_zeroes(int32_t num_1, int32_t num_2)
+{
+    return (0 == num_1 || 0 == num_2) ? true : false;
+}
+
 
 
 int32_t add(int32_t num_1, int32_t num_2)
 {
-    int32_t lessor = find_smaller(num_1, num_2);
     int32_t sum = num_1 + num_2;
     if ((true == check_both_positive(num_1, num_2)) && 0 > sum)
     {
@@ -39,7 +43,6 @@ int32_t add(int32_t num_1, int32_t num_2)
         sum = -1;
     }
     return sum;
-
 }
 
 int32_t subtract(int32_t num_1, int32_t num_2)
@@ -61,19 +64,45 @@ int32_t subtract(int32_t num_1, int32_t num_2)
 
 int32_t multiply(int32_t num_1, int32_t num_2)
 {
-    if ((INT32_MAX < num_1 || INT32_MAX < num_2) || (INT32_MIN > num_1 || INT32_MIN > num_2))
-    {   
-        fprintf(stderr, "Overflow occured.\n");
-        return -1;
+    int32_t product = 0;
+    if (check_for_zeroes(num_1, num_2))
+    {
+        fprintf(stderr, "Multiplcation with 0.\n");
+        product = -1;
     }
+    product = num_1 * num_2;
+    if (check_both_positive(num_1, num_2) && 0 > product)
+    {   
+        fprintf(stderr, "Overflow with two positives.\n");
+        product = -1;
+    } else if (check_both_negative(num_1, num_2) && 0 > product)
+    {
+        fprintf(stderr, "Overflow with two negatives.\n");
+        product = -1;
+    }
+    if ((0 < product && (0 > num_1 || 0 > num_2)) && (product / num_2) != num_1)
+    {
+        fprintf(stderr, "Underflow occured.\n");
+        product = -1;
+    }
+    
+    return product;
 }
 int32_t divide(int32_t num_1, int32_t num_2)
-{
-    if ((INT32_MAX < num_1 || INT32_MAX < num_2) || (INT32_MIN > num_1 || INT32_MIN > num_2))
-    {   
-        fprintf(stderr, "Overflow occured.\n");
-        return -1;
+{   
+    int32_t quotient = 0;
+    if (check_for_zeroes(num_1, num_2))
+    {
+        fprintf(stderr, "Division with 0.\n");
+        quotient = -1;
     }
+    quotient = num_1 / num_2;
+    if (num_1 != quotient * num_2)
+    {
+        fprintf(stderr, "Divide has experienced overflow.\n");
+        quotient = -1;
+    }
+    return quotient;
 }
 int32_t modulo(int32_t num_1, int32_t num_2)
 {
