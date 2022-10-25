a2234e0223d4beef4f07fc18d346670258861ee8
Matthew Pieczynski <matthew.n.pieczynski00@account.cybbh.space>
2022-10-25 08:01:20 -0400

    Add function pointer to clean up code

--- src/calc.c
+++ src/calc.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include "../includes/operands.h"
 
+int32_t evaluate_equation(int32_t (*function)(int32_t, int32_t), int32_t num_1, int32_t num_2)
+{
+    int32_t result = (*function)(num_1, num_2);
+    return result;
+}
+
 int main(int argc, char ** argv)
 {
     int exit_status = EXIT_SUCCESS;
@@ -10,6 +16,29 @@ int main(int argc, char ** argv)
         fprintf(stderr, "Incorrect amount of arguments, 3 needed.\n");
         exit_status = EXIT_FAILURE;
         goto EXIT;
+    }
+    char operand = argv[2][0];
+    int32_t num_1 = 0;
+    int32_t num_2 = 0;
+    char * garbage_bin = NULL;
+    num_1 = strtol(argv[1], &garbage_bin, 10);
+    num_2 = strtol(argv[3], &garbage_bin, 10);
+
+    switch (operand)
+    {
+    case '+':
+        
+        printf("%d + %d = %d\n", num_1, num_2, evaluate_equation(add, num_1, num_2));
+        break;
+    case '-':
+        printf("%d - %d = %d\n", num_1, num_2, subtract(num_1, num_2));
+        break;
+    case '/':
+        printf("%d / %d = %d\n", num_1, num_2, divide(num_1, num_2));
+        break;
+    default:
+        printf("Invalid Operand. %c\n", operand);
+        break;
     }
 
     EXIT:
