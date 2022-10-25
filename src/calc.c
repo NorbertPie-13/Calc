454671aca60b01f5785ca49980a4cf31f038e418
Matthew Pieczynski <matthew.n.pieczynski00@account.cybbh.space>
2022-10-25 10:49:05 -0400

    Add modulo to driver. Needs multiplication and operator escape sequence.

--- src/calc.c
+++ src/calc.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../includes/operands.h"
 
 int32_t evaluate_equation(int32_t (*function)(int32_t, int32_t), int32_t num_1, int32_t num_2)
@@ -16,8 +17,15 @@ int main(int argc, char ** argv)
         fprintf(stderr, "Incorrect amount of arguments, 3 needed.\n");
         exit_status = EXIT_FAILURE;
         goto EXIT;
+    } else if (strlen(argv[2]) > 2)
+    {
+        fprintf(stderr, "Operator is in incorrect format.\n");
+        exit_status = EXIT_FAILURE;
+        goto EXIT;
     }
+    
     char operand = argv[2][0];
+    
     int32_t num_1 = 0;
     int32_t num_2 = 0;
     char * garbage_bin = NULL;
@@ -31,11 +39,22 @@ int main(int argc, char ** argv)
         printf("%d + %d = %d\n", num_1, num_2, evaluate_equation(add, num_1, num_2));
         break;
     case '-':
-        printf("%d - %d = %d\n", num_1, num_2, subtract(num_1, num_2));
+        printf("%d - %d = %d\n", num_1, num_2, evaluate_equation(subtract, num_1, num_2));
         break;
     case '/':
-        printf("%d / %d = %d\n", num_1, num_2, divide(num_1, num_2));
+        printf("%d / %d = %d\n", num_1, num_2, evaluate_equation(divide, num_1, num_2));
         break;
+
+    case '%':
+        if (num_2 == 0 && -1 == evaluate_equation(modulo, num_1, num_2))
+        {
+            exit_status = EXIT_FAILURE;
+        } else
+        {
+            printf("%d %% %d = %d\n", num_1, num_2, evaluate_equation(modulo, num_1, num_2));
+        }
+        
+        break; 
     default:
         printf("Invalid Operand. %c\n", operand);
         break;
