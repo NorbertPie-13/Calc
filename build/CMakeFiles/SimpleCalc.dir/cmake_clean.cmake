4ebc2215d53a74ba78d9308efa8c8699f21ad59c
Matthew Pieczynski <matthew.n.pieczynski00@account.cybbh.space>
2022-11-07 07:28:07 -0500

    add build files

---
+++ build/CMakeFiles/SimpleCalc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleCalc.dir/src/calc.c.o"
+  "CMakeFiles/SimpleCalc.dir/src/operands.c.o"
+  "SimpleCalc"
+  "SimpleCalc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SimpleCalc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
